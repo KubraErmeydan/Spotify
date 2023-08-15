95f91e38f3bee0c4cd8c576de5482505a03332d0
KubraErmeydan <73437863+KubraErmeydan@users.noreply.github.com>
2023-08-15 11:43:38 +0300

    tabbar created

---
+++ lib/views/home.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height* .5,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.white.withOpacity(0.5),
+                Colors.white.withOpacity(0.1),
+                Colors.black.withOpacity(0),
+              ]
+            )
+          ),
+        )
+      ],),
+    );
+  }
+}
