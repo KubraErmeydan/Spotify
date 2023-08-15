862c51d890bdd673c468814dba665d3f019d79f2
KubraErmeydan <73437863+KubraErmeydan@users.noreply.github.com>
2023-08-15 15:21:38 +0300

    home

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -11,7 +11,9 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [
+      body: Stack(
+        alignment: Alignment.topCenter,
+        children: [
         Container(
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height* .5,
@@ -26,8 +28,73 @@ class _HomeViewState extends State<HomeView> {
               ]
             )
           ),
+        ),
+        SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(height: 40,),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Recently Played",style: Theme.of(context).textTheme.headline6,),
+                    SizedBox(width:20 ,),
+                    Row(
+                      children: [
+                        Icon(Icons.history),
+                        SizedBox(width:16 ,),
+                        Icon(Icons.settings),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+
+
+              SingleChildScrollView(
+                padding: EdgeInsets.all(16),
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                  AlbumCard(image:AssetImage('assets/dktt.jpg'), label:'Dolu Kadehi Ters Tut',),
+                    SizedBox(width: 16,),
+                    AlbumCard(image:AssetImage('assets/dragons.jpeg'), label:'Imagine Dragons',),
+                    SizedBox(width: 16,),
+                    AlbumCard(image:AssetImage('assets/lp.jpeg'), label:'LP',),
+                    SizedBox(width: 16,),
+                    AlbumCard(image:AssetImage('assets/mavigri.jpg'), label:'Mavi Gri',),
+                    SizedBox(width: 16,),
+                ],),
+              )
+            ],),
+          ),
         )
       ],),
+    );
+  }
+}
+
+class AlbumCard extends StatelessWidget {
+  final ImageProvider image;
+  final String label;
+  const AlbumCard({
+    super.key, required this.image, required this.label,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image(image: image, width: 120,height: 120,fit: BoxFit.cover),
+        SizedBox(height: 10,),
+        Text(label)
+      ],
     );
   }
 }
